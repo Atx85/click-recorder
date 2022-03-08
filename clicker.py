34696ae0b09f0769fa45d320f6943492d19acd9e
Attila <atx@noreply.com>
2022-03-08 20:09:35 +0000

    added pause option + console message update

--- clicker.py
+++ clicker.py
@@ -2,6 +2,7 @@ import keyboard
 from pynput import mouse
 from pynput.mouse import Button, Controller
 import time
+import os
 
 timerStarted = False
 recordingStarted = False
@@ -9,9 +10,13 @@ recordingStopped = False
 startPlaying = False # init play loop
 stopppedPlaying = False # the loop has finished
 nextEvent = 0 # the next event from the eventTimes array
+timePaused = False
+pausedAt = -1
 t0 = 0
 eventTimes = []
 mouseController = Controller()
+showMessage = True
+iterations = 0
 
 def on_click(x, y, button, pressed):
     global timerStarted
@@ -26,27 +31,42 @@ def on_click(x, y, button, pressed):
         "type": ('Pressed' if pressed else 'Released'),
         "time": actualTime
     })
-    print(eventTimes)  
+    newClick = eventTimes[len(eventTimes) - 1]
+    print("[" + str(newClick['time']) + "] Recorded " + str(newClick['type']) + " at " + str(newClick['x']) + ':' + str(newClick['y']))  
     if not timerStarted:
         timerStarted = True
 
-def on_click_show_pos(x, y, button, pressed):
-    global eventTimes
+def show_message(state):
+    global showMessage
     global nextEvent
-    print(('Pressed' if pressed else 'Released') + ' at ' + str(x) + ':' + str(y))
+    global iterations
+    if showMessage:
+        os.system('cls')
+        if state == 'start':
+            print('Click to start recording! [q]: quit, [s]: stop recording')
+        if state == 'finished recording':
+            print('Press p to start playing the recorded clicks. [q]: quit')
+        if state == 'played click':
+            print('next event: ' + str(nextEvent))
+            print('iterations: ' + str(iterations))
+            for i,c in enumerate(eventTimes):
+                if i == nextEvent - 1:
+                    print(">" + str(c))
+                else: 
+                    print(" " + str(c))
+            print('[q]: quite, [g]: pause [h]: unpause')
+        if state == 'wait':
+            pass
+        showMessage = False
+
 
 listener = mouse.Listener(
     on_click=on_click
 )
 listener.start()
-clickListener = mouse.Listener(
-    on_click=on_click_show_pos
-)
-
-clickListener.start()
-
 def main():
     while True:
+        show_message('start')
         try:
             global timerStarted
             global recordingStarted
@@ -55,6 +75,10 @@ def main():
             global startPlaying
             global nextEvent
             global mouseController
+            global showMessage
+            global timePaused
+            global t0
+            global iterations
 
             if keyboard.is_pressed('q'):
                 listener.stop()
@@ -62,33 +86,42 @@ def main():
             if keyboard.is_pressed('s'):
                 recordingStopped = True
                 listener.stop()
-                # print('recording is stopped press p for play')
-
+                showMessage = True
             if keyboard.is_pressed('p'):
                 startPlaying = True
+
+            if startPlaying:
+                if keyboard.is_pressed('g'):
+                    timePaused = True         
+                if keyboard.is_pressed('h') and timePaused:
+                    timePaused = False
+                    t0 = time.time() + eventTimes[pausedAt]['time']
             if timerStarted and not recordingStarted and not recordingStopped:
-                global t0
-                t0 = time.time()
+                t0 = time.time() - eventTimes[nextEvent - 1]['time']
                 timerStarted = False
                 recordingStarted = True
 
-            if startPlaying:
+            if startPlaying and not timePaused:
                 actualTime = time.time() - t0
                 if stopppedPlaying:
                     t0 = time.time()
                     nextEvent = 0
                     stopppedPlaying = False
-                if not stopppedPlaying:
-                    if (time.time() - t0) > eventTimes[nextEvent]["time"] and ((len(eventTimes) - 1) > nextEvent):
+                if not stopppedPlaying and not timePaused:
+                    if (time.time() - t0) > eventTimes[nextEvent]["time"] and ((len(eventTimes)) > nextEvent):
                         mouseController.position = (float(eventTimes[nextEvent]["x"]),float(eventTimes[nextEvent]["y"]))
-                        print(mouseController.position)
                         if (eventTimes[nextEvent]["type"] == "Pressed"):
                             mouseController.press(Button.left)
+                            showMessage = True
+                            show_message('played click')
                         if (eventTimes[nextEvent]["type"] == "Released"):
                             mouseController.release(Button.left)
+                            showMessage = True
+                            show_message('played click')
                         nextEvent += 1
-                    if len(eventTimes) - 1 <= nextEvent:
+                    if len(eventTimes) < nextEvent + 1:
                         stopppedPlaying = True
+                        iterations += 1
 
 
         except Exception as e:
